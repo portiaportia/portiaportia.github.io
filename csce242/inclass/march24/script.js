e4548d54b507bdf9c20389d024a5b6d1f2301eac
portiaportia <portiaplante@gmail.com>
2020-03-19 08:09:52 -0400

    adding json march 24 example

---
+++ csce242/inclass/march24/script.js
@@ -0,0 +1,41 @@
+async function showShoes(){
+    let response = await fetch('https://portiaportia.github.io/csce242/json/shoes.json');
+    let shoesJSON = await response.json();
+    let shoeDiv = document.getElementById("shoes-section");
+    
+    //loop through the list of shoes from the JSON file
+    for(i in shoesJSON){
+        let shoe = shoesJSON[i];
+        shoeDiv.append(getShoeItem(shoe));
+    }
+
+}
+
+function getShoeItem(shoe){
+    let shoeSection = document.createElement("section");
+    
+    let h3Elem = document.createElement("h3");
+    h3Elem.innerText = shoe.name;
+    shoeSection.append(h3Elem);
+
+    let ulElem = document.createElement("ul");
+    shoeSection.append(ulElem);
+
+    let liBrand = document.createElement("li");
+    liBrand.innerText = `Brand: ${shoe.brand}`;
+    ulElem.append(liBrand);
+
+    let liPrice = document.createElement("li");
+    liPrice.innerText = `Price: $${shoe.price}`;
+    ulElem.append(liPrice);
+
+    let liReviews = document.createElement("li");
+    liReviews.innerText = `Reviews: ${shoe.reviews.join(', ')}`;
+    ulElem.append(liReviews);
+
+    return shoeSection;
+}
+
+window.onload = function(){
+    this.showShoes();
+}
