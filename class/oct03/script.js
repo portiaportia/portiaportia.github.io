7431d6f71f9968390de67aa6cc3b69c06f7c3220
portiaportia <portiaplante@gmail.com>
2023-10-03 11:21:36 -0400

    sdfwe

--- class/oct03/script.js
+++ class/oct03/script.js
@@ -2,6 +2,28 @@ const toggleNav = () => {
   document.getElementById("nav-items").classList.toggle("hide-small");
 };
 
+const setupToyTable = () => {
+  let toyTable = document.getElementById("toy-table");
+
+  let toyList = [];
+  toyList["doll"] = 9.54;
+  toyList["drum"] = 12.99;
+
+  for (let toy in toyList) {
+    let tr = document.createElement("tr");
+    toyTable.append(tr);
+
+    let td1 = document.createElement("td");
+    td1.innerHTML = toy;
+    tr.append(td1);
+    tr.onclick = () => {
+      console.log(toyList[toy]);
+    };
+  }
+};
+
 window.onload = () => {
   document.getElementById("hamburger").onclick = toggleNav;
+
+  setupToyTable();
 };
