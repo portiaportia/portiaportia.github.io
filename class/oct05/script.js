75edcb02ba898f588ed7d8f966a45def8ab64bb2
portiaportia <portiaplante@gmail.com>
2023-10-04 14:21:38 -0400

    updated slideshow to have fade in and out

---
+++ class/oct05/script.js
@@ -0,0 +1,35 @@
+const toggleNav = () => {
+  document.getElementById("nav-items").classList.toggle("hide-small");
+};
+
+let count = 1;
+
+const slide = () => {
+  const currentImage = document.querySelector(
+    `#slideshow :nth-child(${count})`
+  );
+  count++;
+
+  let nextImage = currentImage.nextElementSibling;
+
+  if (nextImage == null) {
+    nextImage = document.querySelector("#slideshow :first-child");
+    count = 1;
+  }
+  currentImage.classList.add("slideHide");
+  nextImage.classList.add("slideShow");
+
+  currentImage.addEventListener("animationend", () => {
+    currentImage.classList.add("hidden");
+    currentImage.classList.remove("slideHide");
+  });
+
+  nextImage.addEventListener("animationend", () => {
+    nextImage.classList.remove("hidden");
+    nextImage.classList.remove("slideShow");
+  });
+};
+
+window.onload = () => {
+  setInterval(slide, 3000);
+};
