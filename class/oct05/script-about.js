ba6081e931d3f205e3646793bfe6f089b04aff6d
portiaportia <portiaplante@gmail.com>
2023-10-05 12:50:44 -0400

    updated oct5

--- class/oct05/script-about.js
+++ class/oct05/script-about.js
@@ -1,5 +1,49 @@
-const toggleNav = () => {
-  document.getElementById("nav-items").classList.toggle("hide-small");
-};
+const slide = (nextImage) => {
+    const currentImage = document.querySelector("#preview :not(.hidden)");
 
-window.onload = () => {};
+    currentImage.classList.add("slideHide");
+    nextImage.classList.add("slideShow");
+
+    currentImage.addEventListener("animationend", () => {
+        currentImage.classList.add("hidden");
+        currentImage.classList.remove("slideHide");
+    });
+
+    nextImage.addEventListener("animationend", () => {
+        nextImage.classList.remove("hidden");
+        nextImage.classList.remove("slideShow");
+    })
+}
+
+const slideForward = () => {
+    const currentImage = document.querySelector("#preview :not(.hidden)");
+    let nextImage = currentImage.nextElementSibling;
+
+    if (nextImage == null) {
+        nextImage = document.querySelector("#preview :first-child");
+    }
+
+    slide(nextImage);
+}
+
+const slideBackward = () => {
+    const currentImage = document.querySelector("#preview :not(.hidden)");
+    let prevImage = currentImage.previousElementSibling;
+
+    if (prevImage == null) {
+        prevImage = document.querySelector("#preview :last-child");
+    }
+    slide(prevImage);
+}
+
+window.onload = () => {
+    document.querySelectorAll("#thumbs img").forEach((thumb, index) => {
+        thumb.onclick = () => {
+            const nextImage = document.querySelector(`#preview :nth-child(${index+1})`);
+            slide(nextImage);
+        }
+    });
+
+    document.getElementById("forward-arrow").onclick = slideForward;
+    document.getElementById("backward-arrow").onclick = slideBackward;
+};
