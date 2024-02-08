3bb8ba3b261f49cab2b7db903251dc9ffa0ae68a
portiaportia <portiaplante@gmail.com>
2024-02-08 09:14:40 -0500

    updated

--- csce242/examples/feb8/script.js
+++ csce242/examples/feb8/script.js
@@ -1,35 +1,23 @@
-let pos = 0;
+const updateThermomether = () => {
+    const goal = 10000;
+    const funds = document.getElementById("txt-funds").value;
+    const errorSpan = document.getElementById("funds-error");
+    errorSpan.innerHTML = "";
+    const therm = document.getElementById("thermometer");
+    document.querySelector(":root").style.setProperty('--funds', "0%");
 
-const colorSquare = () => {
-    document.getElementById("square").classList.add("rainbow");
-    //document.querySelector("#square")
-};
+    /* Error */
+    if (isNaN(funds)) {
+        errorSpan.innerHTML = "* Not a valid number";
+        return;
+    } else if (funds < 0) {
+        errorSpan.innerHTML = "* Negative number";
+        return;
+    }
 
-const moveDown = () => {
-    pos += 10;
-    const root = document.querySelector(":root");
-    root.style.setProperty("--square-top", pos + "px");
-};
-
-const addCircle = () => {
-    //input a valid color
-    const color = document.getElementById("txt-color").value;
-    //change color of all the balls
-    //const root = document.querySelector(":root");
-    //root.style.setProperty("--ball-color", color);
-
-    const playground = document.getElementById("circle-playground");
-
-    //playground.innerHTML += "<section class='circle'></section>";
-
-    const ball = document.createElement("section");
-    playground.append(ball);
-    ball.classList.add("circle");
-    //changes color of 1 ball
-    ball.style.background = color;
+    const percent = funds / goal * 100;
+    document.querySelector(":root").style.setProperty('--funds', percent + "%");
 
 };
 
-document.getElementById("btn-color").onclick = colorSquare;
-document.getElementById("btn-move-down").onclick = moveDown;
-document.getElementById("btn-add-circle").onclick = addCircle;
+document.getElementById("btn-cont").onclick = updateThermomether;
