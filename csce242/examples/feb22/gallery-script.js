7a4f6971cb2d433ea865438491147cb7b1c748f5
portiaportia <portiaplante@gmail.com>
2024-02-27 09:08:02 -0500

    updated

--- csce242/examples/feb22/gallery-script.js
+++ csce242/examples/feb22/gallery-script.js
@@ -3,6 +3,28 @@ document.querySelectorAll(".items section").forEach((section) => {
     section.onclick = (e) => {
         document.getElementById("dialog").style.display = "block";
 
+        //const src = section.querySelector("img").src;
+        //const src = e.currentTarget.querySelector("img").src;
+        //console.log(src);
+
         document.querySelector("#dialog-content img").src = e.currentTarget.querySelector("img").src;
+
+        const detailsSection = document.getElementById("dialog-details");
+        detailsSection.innerHTML = "";
+        const h3 = document.createElement("h3");
+        h3.innerHTML = section.querySelector("h3").innerHTML;
+        detailsSection.append(h3);
+
+        const ps = section.querySelectorAll("p");
+
+        ps.forEach((p) => {
+            const newP = document.createElement("p");
+            newP.innerHTML = p.innerHTML;
+            detailsSection.append(newP);
+        });
     };
 });
+
+document.getElementById("dialog-close").onclick = () => {
+    document.getElementById("dialog").style.display = "none";
+};
