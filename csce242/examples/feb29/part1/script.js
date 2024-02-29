11cb79939af36bb8fa7b8e633a712903d22d5c28
portiaportia <portiaplante@gmail.com>
2024-02-29 09:22:28 -0500

    updated shoes

--- csce242/examples/feb29/part1/script.js
+++ csce242/examples/feb29/part1/script.js
@@ -1,1 +1,43 @@
-//"https://portiaportia.github.io/json/shoes.json"
+const getShoes = async() => {
+    const url = "https://portiaportia.github.io/json/shoes.json";
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const showShoes = async() => {
+    const shoes = await getShoes();
+
+    shoes.forEach((shoe) => {
+        document.getElementById("shoes-section").append(getShoeSection(shoe));
+    });
+};
+
+const getShoeSection = (shoe) => {
+    const section = document.createElement("section");
+    const h3 = document.createElement("h3");
+    h3.innerHTML = shoe.name;
+    section.append(h3);
+
+    const brandP = document.createElement("p");
+    brandP.innerHTML = shoe.brand;
+    section.append(brandP);
+
+    const ul = document.createElement("ul");
+    section.append(ul);
+
+    shoe.reviews.forEach((review) => {
+        const li = document.createElement("li");
+        li.innerHTML = review;
+        ul.append(li);
+    });
+
+
+    return section;
+}
+
+showShoes();
