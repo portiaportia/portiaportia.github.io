25e7d6d285d6ee10b5d24d432c7d58ad4bef0595
portiaportia <portiaplante@gmail.com>
2024-05-15 11:27:36 -0400

    udpated

--- csce242/exercises/05-js-toggle/script.js
+++ csce242/exercises/05-js-toggle/script.js
@@ -2,3 +2,32 @@
 document.getElementById("toggle").onclick = () => {
     document.getElementById("nav-items").classList.toggle("hide-small");
 }
+
+const moveBall = () => {
+    const root = document.querySelector(":root");
+    root.style.setProperty("--ball-left", ballLeft + "px");
+    root.style.setProperty("--ball-top", ballTop + "px");
+}
+
+//moving to the right
+let ballLeft = 0;
+let ballTop = 0;
+document.getElementById("right-arrow").onclick = () => {
+    ballLeft += 10;
+    moveBall();
+}
+
+document.getElementById("left-arrow").onclick = () => {
+    ballLeft -= 10;
+    moveBall();
+}
+
+document.getElementById("top-arrow").onclick = () => {
+    ballTop -= 10;
+    moveBall();
+}
+
+document.getElementById("bottom-arrow").onclick = () => {
+    ballTop += 10;
+    moveBall();
+}
