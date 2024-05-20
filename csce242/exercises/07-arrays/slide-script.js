91ff7baf1700ba7df810f8cfa20f80992848d4f1
portiaportia <portiaplante@gmail.com>
2024-05-20 11:44:31 -0400

    updated slide show

--- csce242/exercises/07-arrays/slide-script.js
+++ csce242/exercises/07-arrays/slide-script.js
@@ -7,10 +7,36 @@ const slide = (nextImage) => {
 
 }
 
+//slide forward
+document.getElementById("forward-arrow").onclick = () => {
+    const currentImage = document.querySelector("#preview :not(.hidden)");
+    let nextImage = currentImage.nextElementSibling;
+
+    //if we get to the end of the list
+    if(nextImage == null){
+        nextImage = document.querySelector("#preview :first-child");
+    }
+
+    slide(nextImage);
+};
+
+//slide backward
+document.getElementById("backward-arrow").onclick = () => {
+    const currentImage = document.querySelector("#preview :not(.hidden)");
+    let nextImage = currentImage.previousElementSibling;
+
+    //if we get to the end of the list
+    if(nextImage == null){
+        nextImage = document.querySelector("#preview :last-child");
+    }
+
+    slide(nextImage);
+}
+
+//slide to a clicked thumb
 document.querySelectorAll("#thumbs img").forEach((thumb, index)=>{
     thumb.onclick = () => {
-        console.log(index);
-        const nextImage = document.querySelector("#preview :nth-child(2)");
+        const nextImage = document.querySelector(`#preview :nth-child(${index+1})`);
         slide(nextImage);
     };
 });
