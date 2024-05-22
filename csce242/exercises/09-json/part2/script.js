5038471412c846795f7b3ce664a99474cedb9fea
portiaportia <portiaplante@gmail.com>
2024-05-22 11:31:12 -0400

    udated

--- csce242/exercises/09-json/part2/script.js
+++ csce242/exercises/09-json/part2/script.js
@@ -1,43 +1,49 @@
-//https://api.openbrewerydb.org/breweries
-
-const getBreweries = async() => {
+const getBreweries = async () => {
     try {
-        return (await fetch("https://api.openbrewerydb.org/breweries")).json();
-    } catch(error){
-        console.log(error);
+      return (await fetch("https://api.openbrewerydb.org/breweries")).json();
+    } catch (error) {
+      console.log(error);
     }
-}
-
-const getMap = async(lat, long) => {
+  };
+  
+  const getMap = async (latitude, longitude) => {
     try {
-        return fetch(`https://maps.google.com/maps?q=${lat},${long}&t=&z=15&ie=UTF8&iwloc=&&output=embed`
-        ).url;
-    } catch(error){
-        console.log(error);
+      return (
+        await fetch(
+          `https://maps.google.com/maps?q=${latitude},${longitude}&t=&z=15&ie=UTF8&iwloc=&&output=embed`
+        )
+      ).url;
+    } catch (error) {
+      console.log(error);
     }
-}
-
-const showBreweries = async() => {
+  };
+  
+  const showBreweries = async () => {
     const breweries = await getBreweries();
     const breweriesList = document.getElementById("breweries-section");
-
-    breweries.forEach((brewery)=> {
-        const section = document.createElement("section");
-        section.classList.add("brewery");
-
-        const h3 = document.createElement("h3");
-        section.append(h3);
-        const a = document.createElement("a");
-        h3.append(a);
-        a.innerHTML = brewery.name;
-        a.href = brewery.website_url;
-
-        const p = document.createElement("p");
-        section.append(p);
-        p.innerHTML = `${brewery.brewery_type} brewery`;
-
-        breweriesList.append(section);
+  
+    breweries.forEach(async (brewery) => {
+      const section = document.createElement("section");
+      breweriesList.append(section);
+      section.classList.add("brewery");
+  
+      const a = document.createElement("a");
+      a.href = brewery.website_url;
+      section.append(a);
+  
+      const h3 = document.createElement("h3");
+      a.append(h3);
+      h3.innerHTML = brewery.name;
+  
+      const p = document.createElement("p");
+      a.append(p);
+      p.innerHTML = `${brewery.brewery_type} brewery`;
+  
+      const iframe = document.createElement("iframe");
+      iframe.src = await getMap(brewery.latitude, brewery.longitude);
+      a.append(iframe);
     });
-}
-
-showBreweries();
+  };
+  
+  window.onload = showBreweries();
+  
