80963849572b36dfcb2b6a2134b0681bcae22ab5
portiaportia <portiaplante@gmail.com>
2024-05-22 11:23:57 -0400

    updated

---
+++ csce242/exercises/09-json/part2/script.js
@@ -0,0 +1,34 @@
+//https://api.openbrewerydb.org/breweries
+
+const getBreweries = async() => {
+    try {
+        return (await fetch("https://api.openbrewerydb.org/breweries")).json();
+    } catch(error){
+        console.log(error);
+    }
+}
+
+const showBreweries = async() => {
+    const breweries = await getBreweries();
+    const breweriesList = document.getElementById("breweries-section");
+
+    breweries.forEach((brewery)=> {
+        const section = document.createElement("section");
+        section.classList.add("brewery");
+
+        const h3 = document.createElement("h3");
+        section.append(h3);
+        const a = document.createElement("a");
+        h3.append(a);
+        a.innerHTML = brewery.name;
+        a.href = brewery.website_url;
+
+        const p = document.createElement("p");
+        section.append(p);
+        p.innerHTML = `${brewery.brewery_type} brewery`;
+
+        breweriesList.append(section);
+    });
+}
+
+showBreweries();
