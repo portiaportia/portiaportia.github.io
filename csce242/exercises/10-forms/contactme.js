b12a82a74a2466fdaab7403371fe1850bfaaafb1
portiaportia <portiaplante@gmail.com>
2024-05-23 11:28:23 -0400

    works

--- csce242/exercises/10-forms/contactme.js
+++ csce242/exercises/10-forms/contactme.js
@@ -1,32 +1,40 @@
-//https://api.web3forms.com/submit
+const showEmailResult = async(e) => {
+    e.preventDefault();
+
+    const result = document.getElementById("result");
+    let response = await getEmailResult();
+
+    if(response.status == 200){
+        result.innerHTML = "Email successfully sent";
+    } else {
+        result.innerHTML = "Sorry, your email was not sent";
+    }
+
+};
 
 const getEmailResult = async() => {
     const form = document.getElementById("contact-form");
     const formData = new FormData(form);
     const object = Object.fromEntries(formData);
     const json = JSON.stringify(object);
-
-    //show please wait message
     const result = document.getElementById("result");
     result.innerHTML = "Please wait....";
 
     try {
-        const response = await fetch("https://api.web3forms.com/submit",{
-            method:"POST",
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
             headers:{
                 "Content-Type":"application/json",
                 Accept:"application/json"
             },
             body:json
         });
+
+        return response;
     } catch(error){
         console.log(error);
         result.innerHTML = "Sorry, your email couldn't be sent";
     }
-}
+};
 
-document.getElementById("contact-form").onsubmit = async(e) => {
-    e.preventDefault();
-
-    getEmailResult();
-}
+document.getElementById("contact-form").onsubmit = showEmailResult;
