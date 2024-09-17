c562c14f41b47b955e0064b66d2151ef5751a849
portiaportia <portiaplante@gmail.com>
2024-09-17 12:19:47 -0400

    sdfas

--- csce242-fall2024/exercises/07_js_nav/script.js
+++ csce242-fall2024/exercises/07_js_nav/script.js
@@ -1,2 +1,6 @@
 /* When the hamburger is clicked, toggle between showing
 and hiding the nav items */
+//document.getElementById("toggle-nav")
+document.querySelector("#toggle-nav").onclick = () => {
+    document.getElementById("nav-items").classList.toggle("hidden-small");
+};
