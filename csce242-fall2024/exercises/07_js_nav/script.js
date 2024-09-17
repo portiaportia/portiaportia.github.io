a432aaab161138e56b7687a89d447f62ccf435b8
portiaportia <portiaplante@gmail.com>
2024-09-17 12:29:48 -0400

    keeping everyone updated

--- csce242-fall2024/exercises/07_js_nav/script.js
+++ csce242-fall2024/exercises/07_js_nav/script.js
@@ -4,3 +4,9 @@ and hiding the nav items */
 document.querySelector("#toggle-nav").onclick = () => {
     document.getElementById("nav-items").classList.toggle("hidden-small");
 };
+
+/* Don't go to links desitination when clicked */
+document.getElementById("click-link").onclick = (event) => {
+    event.preventDefault(); //don't go to the links destination
+    console.log("you clicked a link");
+};
