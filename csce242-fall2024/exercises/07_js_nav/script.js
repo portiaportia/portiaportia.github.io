cbe92414b70558d81e84981f614a065789ac3c38
portiaportia <portiaplante@gmail.com>
2024-09-19 12:30:58 -0400

    updated

--- csce242-fall2024/exercises/07_js_nav/script.js
+++ csce242-fall2024/exercises/07_js_nav/script.js
@@ -23,3 +23,43 @@ document.getElementById("move-down").onclick = () => {
     pos++;
     document.getElementById("ball").style.setProperty("top", pos + "px");
 }
+
+/* Show color message */
+document.getElementById("btn-show-color").onclick = () => {
+    const color = document.getElementById("txt-enter-color").value.toLowerCase().trim();
+    const messageP = document.getElementById("color-message");
+    let mood = "";
+
+    if(color == "red"){
+        mood = "angry";
+    }
+    else if(color == "blue"){
+        mood = "moody";
+    }
+    else {
+        mood = "undefined";
+    }
+
+    messageP.innerHTML = `You chose ${color} so you are ${mood}`;
+}
+
+/* Donations */
+/* Get the number
+- If it is not a number or it is negative, show an error in the error
+Otherwise First show the percent out of 10000 towards goal
+Next show a visual representation with a box and a gradient */
+document.getElementById("btn-donate").onclick = () => {
+    const goal = 10000;
+    const donation = document.getElementById("txt-donations").value;
+    const error = document.getElementById("error-donation");
+    error.innerHTML = "";   //clear previous error
+
+    //if we got an invalid donation -> not a number or less than 0
+    if(isNaN(donation) || donation < 0){
+        error.innerHTML = "* Invalid";
+        return;
+    }
+   
+    const percentGoal = donation / goal * 100;
+    console.log(percentGoal + "%");
+}
