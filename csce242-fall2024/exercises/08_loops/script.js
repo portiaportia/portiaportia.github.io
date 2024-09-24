0940758e3df1ccfc5c63e124106e160fe6509f02
portiaportia <portiaplante@gmail.com>
2024-09-24 12:52:55 -0400

    first for loop

--- csce242-fall2024/exercises/08_loops/script.js
+++ csce242-fall2024/exercises/08_loops/script.js
@@ -4,3 +4,19 @@ and hiding the nav items */
 document.querySelector("#toggle-nav").onclick = () => {
   document.getElementById("nav-items").classList.toggle("hidden-small");
 };
+
+document.getElementById("btn-loop").onclick = () => {
+  const ul = document.getElementById("numbers");
+
+  for(let i = 0; i < 10; i++){
+    const li = document.createElement("li");
+    li.innerHTML = i+1;
+    ul.append(li);
+    li.setAttribute("id", "li" + i);
+
+    li.onclick = () => {
+      console.log(`Lucky number ${i+1}`);
+    };
+  }
+}
+
