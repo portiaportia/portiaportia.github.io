6ce8d07ca697932b21275893bb4091ee2eedd9d6
portiaportia <portiaplante@gmail.com>
2024-10-01 12:41:46 -0400

    gallery second version

--- csce242-fall2024/exercises/09_gallery/gallery-script.js
+++ csce242-fall2024/exercises/09_gallery/gallery-script.js
@@ -1,5 +1,6 @@
 //every one second go through and move the class of highlighted threw the gallery
-setInterval(()=>{
+//perfered solution
+/*setInterval(()=>{
     let currentSection = document.querySelector(".items section.highlighted");
 
     //highlight the first item
@@ -17,4 +18,23 @@ setInterval(()=>{
 
     currentSection.classList.remove("highlighted");
     nextSection.classList.add("highlighted");
-}, 500);
+}, 500);*/
+
+let count = 1; 
+
+setInterval(()=>{
+    //remove highlighted from every section
+    document.querySelectorAll(".items section").forEach((section)=>{
+        section.classList.remove("highlighted");
+    });
+
+    //add highlighting to the section with the current count
+    document.querySelector(`.items section:nth-child(${count})`).classList.add("highlighted");
+
+    count++;
+
+    //if you reached the end of the list of sections
+    if(count > document.querySelectorAll(".items section").length){
+        count = 1;
+    }
+},500);
