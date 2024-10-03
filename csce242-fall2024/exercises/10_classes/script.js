5a851779ef67b87601b7e05809035ad558bcac8b
portiaportia <portiaplante@gmail.com>
2024-10-03 11:59:43 -0400

    added dog info

--- csce242-fall2024/exercises/10_classes/script.js
+++ csce242-fall2024/exercises/10_classes/script.js
@@ -14,3 +14,7 @@ const dogs = [];
 dogs.push(new Dog("Coco", "Morkie", "Black", 5, "small", "yorkie.jpg"));
 dogs.push(new Dog("Sam", "Golden", "Yellow", 1, "med", "golden-retriever.jpg"));
 dogs.push(new Dog("Gerald","Pit Bull", "White", 3, "lg", "pitt-bull.jpg"));
+
+dogs.forEach((dog)=>{
+    console.log(dog);
+});
