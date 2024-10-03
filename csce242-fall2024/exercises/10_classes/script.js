7d9890103044f211897b3fd9ecea13a3f68eaf67
portiaportia <portiaplante@gmail.com>
2024-10-03 11:54:23 -0400

    dogs

---
+++ csce242-fall2024/exercises/10_classes/script.js
@@ -0,0 +1,13 @@
+class Dog {
+    constructor(title, breed, color, age, size, pic){
+        this.title = title;
+        this.breed = breed;
+        this.color = color;
+        this.age = age;
+        this.size = size;
+        this.pic = pic;
+    }
+}
+
+const dogs = [];
+//const myDoc = new Dog("molly", "pit bull", "brown", 4, "xs", "images/molly.jpg");
