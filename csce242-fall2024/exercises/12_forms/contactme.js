ba986b1092649bda2a35392cd70caad7721a4306
portiaportia <portiaplante@gmail.com>
2024-10-15 10:55:53 -0400

    forms

---
+++ csce242-fall2024/exercises/12_forms/contactme.js
@@ -0,0 +1,17 @@
+/*
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json",
+                Accept:"application/json"
+            },
+            body:json
+        });
+
+        return response;
+    } catch(error){
+        console.log(error);
+        result.innerHTML = "Sorry, your email couldn't be sent";
+    }
+        */
