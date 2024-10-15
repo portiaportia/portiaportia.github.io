e6e94577a43708d96d4f0f93360dd0f5f32a2bc5
portiaportia <portiaplante@gmail.com>
2024-10-15 12:44:56 -0400

    contact form

--- csce242-fall2024/exercises/12_forms/contactme.js
+++ csce242-fall2024/exercises/12_forms/contactme.js
@@ -1,17 +1,36 @@
-/*
-    try {
-        const response = await fetch("https://api.web3forms.com/submit", {
-            method: "POST",
-            headers:{
-                "Content-Type":"application/json",
-                Accept:"application/json"
-            },
-            body:json
-        });
+    const sendEmail = async(json) => {
+        try {
+            const response = await fetch("https://api.web3forms.com/submit", {
+                method: "POST",
+                headers:{
+                    "Content-Type":"application/json",
+                    Accept:"application/json"
+                },
+                body:json
+            });
+    
+            return response;
+        } catch(error){
+            console.log(error);
+            result.innerHTML = "Sorry, your email couldn't be sent";
+        }
+    };
 
-        return response;
-    } catch(error){
-        console.log(error);
-        result.innerHTML = "Sorry, your email couldn't be sent";
+    document.getElementById("contact-form").onsubmit = async(e) => {
+        e.preventDefault();
+
+        const form = e.target;
+        const formData = new FormData(form);
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+        const result = document.getElementById("result");
+        result.innerHTML = "Sending...";
+
+        const httpResult = await sendEmail(json);
+
+        if(httpResult.status == 200){
+            result.innerHTML = "Email successfully sent";
+        } else {
+            result.innerHTML = "Sorry, your email wasn't sent";
+        }
     }
-        */
