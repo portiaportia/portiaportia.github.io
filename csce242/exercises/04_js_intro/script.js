e89cfa03f5d6ca02108ab06b82c6069f204c750f
portiaportia <portiaplante@gmail.com>
2025-02-04 09:37:16 -0500

    js beginning

---
+++ csce242/exercises/04_js_intro/script.js
@@ -0,0 +1,6 @@
+//do stuff when button clicked
+document.getElementById("btn-click-me").onclick = () => {
+    console.log("WOW!");
+    document.getElementById("result").innerHTML="Hi Portia";
+};
+
