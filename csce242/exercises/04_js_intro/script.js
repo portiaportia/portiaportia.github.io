57c605ea9789b7379597f689f7d04a8c2506744a
portiaportia <portiaplante@gmail.com>
2025-02-04 09:43:24 -0500

    add class

--- csce242/exercises/04_js_intro/script.js
+++ csce242/exercises/04_js_intro/script.js
@@ -4,3 +4,11 @@ document.getElementById("btn-click-me").onclick = () => {
     document.getElementById("result").innerHTML="Hi Portia";
 };
 
+//change color
+document.getElementById("btn-color").onclick = () => {
+    const messageP = document.getElementById("message");
+    messageP.innerHTML = "Good bye";
+    messageP.classList.toggle("sad");
+
+}
+
