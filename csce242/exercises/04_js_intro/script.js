d2044da4e78c01c6124a90d97664020fcc73c8ee
portiaportia <portiaplante@gmail.com>
2025-02-11 09:08:02 -0500

    navigation toggling

--- csce242/exercises/04_js_intro/script.js
+++ csce242/exercises/04_js_intro/script.js
@@ -40,3 +40,6 @@ document.getElementById("txt-emotion").onkeyup = (event) => {
 }
 
 //toggling the nav
+document.getElementById("toggle-nav").onclick = () => {
+    document.getElementById("nav-items").classList.toggle("hide-small");
+};
