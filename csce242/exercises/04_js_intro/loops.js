a500605be8830ba52f517c1abf1e634e207c96db
portiaportia <portiaplante@gmail.com>
2025-02-18 09:05:53 -0500

    error

--- csce242/exercises/04_js_intro/loops.js
+++ csce242/exercises/04_js_intro/loops.js
@@ -1,3 +1,11 @@
 document.getElementById("btn-display").onclick = () => {
-    console.log("hi");
+    const startNum = document.getElementById("txt-start").value;
+    const endNum = document.getElementById("txt-end").value;
+    const errorDisplay = document.getElementById("error-num-order");
+    errorDisplay.innerHTML = "";    //clear out any previous errors
+
+    if(endNum < startNum) {
+        errorDisplay.innerHTML = `${endNum} is less than ${startNum}.`
+        return; //don't do anything else if you already have an error
+    }
 }
