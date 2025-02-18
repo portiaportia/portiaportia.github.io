87b925dc84c53f25c855b8deeb8ce96e066e21bc
portiaportia <portiaplante@gmail.com>
2025-02-18 09:13:12 -0500

    parseInt

--- csce242/exercises/04_js_intro/loops.js
+++ csce242/exercises/04_js_intro/loops.js
@@ -1,6 +1,6 @@
 document.getElementById("btn-display").onclick = () => {
-    const startNum = document.getElementById("txt-start").value;
-    const endNum = document.getElementById("txt-end").value;
+    const startNum = parseInt(document.getElementById("txt-start").value);
+    const endNum = parseInt(document.getElementById("txt-end").value);
     const displayArea = document.getElementById("start-end-display");
     const errorDisplay = document.getElementById("error-num-order");
     errorDisplay.innerHTML = "";    //clear out any previous errors
@@ -8,5 +8,9 @@ document.getElementById("btn-display").onclick = () => {
     if(endNum < startNum) {
         errorDisplay.innerHTML = `${endNum} is less than ${startNum}.`
         return; //don't do anything else if you already have an error
+    }
+
+    for(let i=startNum; i < endNum; i++){
+        console.log(i);
     }
 }
