d2f755cdd0c1e34fa63d69ccdabb10e47c2ba98d
portiaportia <portiaplante@gmail.com>
2025-02-18 09:28:58 -0500

    li made

--- csce242/exercises/04_js_intro/loops.js
+++ csce242/exercises/04_js_intro/loops.js
@@ -2,15 +2,24 @@ document.getElementById("btn-display").onclick = () => {
     const startNum = parseInt(document.getElementById("txt-start").value);
     const endNum = parseInt(document.getElementById("txt-end").value);
     const displayArea = document.getElementById("start-end-display");
+    displayArea.innerHTML = "";
     const errorDisplay = document.getElementById("error-num-order");
     errorDisplay.innerHTML = "";    //clear out any previous errors
+    const favMessageP = document.getElementById("fav-message");
+    favMessageP.innerHTML = "";
 
     if(endNum < startNum) {
         errorDisplay.innerHTML = `${endNum} is less than ${startNum}.`
         return; //don't do anything else if you already have an error
     }
 
-    for(let i=startNum; i < endNum; i++){
-        console.log(i);
+    for(let i=startNum; i <= endNum; i++){
+        const li = document.createElement("li");
+        li.innerHTML = i;
+        displayArea.append(li);
+        //attach event to onclick li
+        li.onclick = () => {
+            console.log("I clicked one!");
+        }
     }
 }
