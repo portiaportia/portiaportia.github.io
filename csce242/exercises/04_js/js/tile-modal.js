5b3a5c75d069605a9a17eeed7fec9cc781312cb0
portiaportia <portiaplante@gmail.com>
2025-02-27 09:30:39 -0500

    popup

--- csce242/exercises/04_js/js/tile-modal.js
+++ csce242/exercises/04_js/js/tile-modal.js
@@ -2,6 +2,8 @@ document.querySelectorAll(".gallery section").forEach((tile)=>{
     tile.onclick = () => {
         const label = tile.querySelector("h4").innerHTML;
         const imgSRC = tile.querySelector("img").src;
-        console.log(label);
+        document.getElementById("popup").style.display="block";
+        document.querySelector("#popup h2").innerHTML = label;
+        document.querySelector("#popup img").src = imgSRC;
     }
 });
