b915f4d4fa173d9f5a49cb944555d3cb18824a9a
portiaportia <portiaplante@gmail.com>
2025-03-03 14:25:53 -0500

    added getMap

--- csce242/exercises/04_js/js/json-breweries.js
+++ csce242/exercises/04_js/js/json-breweries.js
@@ -1,1 +1,13 @@
 //https://api.openbrewerydb.org/breweries
+
+const getMap = async(latitude, longitude) => {
+    try {
+        return (
+            await fetch(
+                `https://maps.google.com/maps?q=${latitude},${longitude}&t=&z=15&ie=UTF8&iwloc=&&output=embed`
+            )
+        ).url;
+    } catch (error) {
+        console.log(error);
+    }
+};
