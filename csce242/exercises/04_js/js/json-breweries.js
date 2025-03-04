c1ffb731482283523ad6f9212a771b62cd4dbee8
portiaportia <portiaplante@gmail.com>
2025-03-04 09:33:31 -0500

    pushing

--- csce242/exercises/04_js/js/json-breweries.js
+++ csce242/exercises/04_js/js/json-breweries.js
@@ -11,3 +11,36 @@ const getMap = async(latitude, longitude) => {
         console.log(error);
     }
 };
+
+const getBreweries = async() => {
+    const url = "https://api.openbrewerydb.org/breweries";
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch(error){
+        console.log(error);
+    }
+};
+
+const showBreweries = async() => {
+    const breweriesSection = document.getElementById("breweries");
+    const breweries = await getBreweries();
+    
+    breweries.forEach((brewery)=> {
+        const section = document.createElement("section");
+        section.classList.add("brewery");
+        breweriesSection.append(section);
+
+        const h3 = document.createElement("h3");
+        h3.innerHTML = brewery.name;
+        section.append(h3);
+        
+        const p = document.createElement("p");
+        p.innerHTML = brewery.brewery_type + " brewery";
+        section.append(p);
+    });
+    
+};
+
+showBreweries();
