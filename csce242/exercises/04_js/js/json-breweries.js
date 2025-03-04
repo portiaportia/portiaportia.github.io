801079805fc81f5bacd972055803bda9c77b79e3
portiaportia <portiaplante@gmail.com>
2025-03-04 09:40:12 -0500

    iframe

--- csce242/exercises/04_js/js/json-breweries.js
+++ csce242/exercises/04_js/js/json-breweries.js
@@ -27,18 +27,27 @@ const showBreweries = async() => {
     const breweriesSection = document.getElementById("breweries");
     const breweries = await getBreweries();
     
-    breweries.forEach((brewery)=> {
+    breweries.forEach(async(brewery)=> {
         const section = document.createElement("section");
         section.classList.add("brewery");
         breweriesSection.append(section);
 
+        const a = document.createElement("a");
+        a.href = brewery.website_url;
+        section.append(a);
+
         const h3 = document.createElement("h3");
         h3.innerHTML = brewery.name;
-        section.append(h3);
+        a.append(h3);
         
         const p = document.createElement("p");
         p.innerHTML = brewery.brewery_type + " brewery";
         section.append(p);
+
+        const iframe = document.createElement("iframe");
+        iframe.src= await getMap(brewery.longitude, brewery.latitude);
+        section.append(iframe);
+
     });
     
 };
