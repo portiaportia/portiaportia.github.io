eb38e2e5d2a3a6c8e4c3ce7bbc0d6abb6a90c1d6
portiaportia <portiaplante@gmail.com>
2025-05-27 10:49:17 -0400

    stuff

---
+++ csce242/examples/04js/js/loops.js
@@ -0,0 +1,8 @@
+document.getElementById("btn-basic-loop").onclick=() => {
+    const loopsDiv = document.getElementById("basic-loops-list");
+    loopsDiv.innerHTML = "Hello";
+    
+    for(let i=1; i <= 5; i++ ){
+        
+    }
+};
