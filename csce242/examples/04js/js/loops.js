9521d92df2266c51400d36f29806606e364779c7
portiaportia <portiaplante@gmail.com>
2025-05-27 10:52:14 -0400

    catchup

--- csce242/examples/04js/js/loops.js
+++ csce242/examples/04js/js/loops.js
@@ -1,8 +1,9 @@
 document.getElementById("btn-basic-loop").onclick=() => {
     const loopsDiv = document.getElementById("basic-loops-list");
-    loopsDiv.innerHTML = "Hello";
     
     for(let i=1; i <= 5; i++ ){
-        
+        const p = document.createElement("p");
+        p.innerHTML = i;
+        loopsDiv.append(p);
     }
 };
