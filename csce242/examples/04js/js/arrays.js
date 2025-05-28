3da7d33f98fd42f2eeadf89fe787d48452c1a4af
portiaportia <portiaplante@gmail.com>
2025-05-28 11:48:15 -0400

    awer

--- csce242/examples/04js/js/arrays.js
+++ csce242/examples/04js/js/arrays.js
@@ -1,1 +1,8 @@
-console.log("arrays");
+document.getElementById("btn-show-toys").onclick = () => {
+    let toys = ["drum", "ball", "holla hope", "barbie", "truck"];
+
+    //iterate over using a for loop
+    /*for(let i=0; i < toys.length; i++){
+        console.log(toys[i]);
+    }*/
+}
