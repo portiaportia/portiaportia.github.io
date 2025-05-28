61430b7a399b0d90bb857ea712252bdb5adef10f
portiaportia <portiaplante@gmail.com>
2025-05-28 11:38:04 -0400

    second slide show

--- csce242/examples/04js/js/slide-arrows.js
+++ csce242/examples/04js/js/slide-arrows.js
@@ -11,6 +11,19 @@ document.getElementById("right-arrow").onclick = (e) => {
     switchSlides(currentSlide, nextSlide);
 };
 
+document.getElementById("left-arrow").onclick = (e) => {
+    e.preventDefault(); //prevent going to links destination
+    const currentSlide = getCurrentSlide();
+    let prevSlide = currentSlide.previousElementSibling;
+
+    //if we got to the end of the slideshow
+    if(prevSlide == null){
+        prevSlide = document.querySelector("#slides :last-child");
+    }
+
+    switchSlides(currentSlide, prevSlide);
+};
+
 const getCurrentSlide = () => {
     return document.querySelector("#slides :not(.hidden)");
 }
