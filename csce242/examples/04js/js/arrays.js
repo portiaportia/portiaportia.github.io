c8519404259c4ecc1bf7080b7e1ff3b0449499d7
portiaportia <portiaplante@gmail.com>
2025-05-28 12:04:40 -0400

    awer

--- csce242/examples/04js/js/arrays.js
+++ csce242/examples/04js/js/arrays.js
@@ -17,5 +17,24 @@ document.getElementById("btn-show-toys").onclick = () => {
 
 //when button clicked we will make an array table
 document.getElementById("btn-show-toy-table").onclick = () => {
-    console.log("sdfew");
+    let toys = [];
+    toys["Drum"] = 20.99;
+    toys["Barbie"] = 9.85;
+    toys["Truck"] = 50.99;
+    toys["Sticker"] = 1.99;
+    toys["Doll"] = 3.99;
+
+    const table = document.getElementById("toy-table");
+
+    for(let toy in toys){
+        const price = toys[toy];
+        let tr = document.createElement("tr");
+        table.append(tr);
+        let td1 = document.createElement("td");
+        td1.innerHTML = toy;
+        tr.append(td1);
+        let td2 = document.createElement("td");
+        td2.innerHTML = price;
+        tr.append(td2);
+    }
 };
