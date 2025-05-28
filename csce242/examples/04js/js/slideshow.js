3403b5bb3c7ba877f506b292586a0375cbf4ee19
portiaportia <portiaplante@gmail.com>
2025-05-28 10:59:11 -0400

    slideshow

--- csce242/examples/04js/js/slideshow.js
+++ csce242/examples/04js/js/slideshow.js
@@ -1,3 +1,13 @@
 setInterval(()=>{
-    console.log("hello");
+    const currentSlide = document.querySelector("#slideshow :not(.hidden)");
+    let nextSlide = currentSlide.nextElementSibling;
+
+    //reached the end
+    if(nextSlide == null) {
+        nextSlide = document.querySelector("#slideshow :first-child");
+    }
+
+    //make current one hidden and next one show
+    currentSlide.classList.add("hidden");
+    nextSlide.classList.remove("hidden");
 }, 1000);
