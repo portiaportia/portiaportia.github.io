6285fd4f6e33af816e9d6b65a484880877d5f483
portiaportia <portiaplante@gmail.com>
2025-05-29 11:32:26 -0400

    awer

--- csce242/examples/04js/js/dog-classes.js
+++ csce242/examples/04js/js/dog-classes.js
@@ -7,9 +7,19 @@ class Dog {
         this.size = size;
         this.pic = pic;
     }
+
+    get item(){
+        const p = document.createElement("p");
+        p.innerHTML = this.title;
+        return p;
+    }
 }
 
 const dogs = [];
 dogs.push(new Dog("Coco", "Morkie", "Black", 5, "small", "yorkie.jpg"));
 dogs.push(new Dog("Sam", "Golden", "Yellow", 1, "Med", "golden-retriever.jpg"));
 dogs.push(new Dog("Gerald", "Pit Bull", "White", 3, "Large", "pitt-bull.jpg"));
+
+dogs.forEach((dog)=>{
+    document.getElementById("dog-house").append(dog.item);
+});
