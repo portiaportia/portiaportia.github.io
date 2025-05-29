52947d71195671771d9f78b1c930900dab2f81cb
portiaportia <portiaplante@gmail.com>
2025-05-29 11:20:04 -0400

    awer

--- csce242/examples/04js/js/slide-arrows.js
+++ csce242/examples/04js/js/slide-arrows.js
@@ -24,6 +24,16 @@ document.getElementById("left-arrow").onclick = (e) => {
     switchSlides(currentSlide, prevSlide);
 };
 
+document.querySelectorAll("#thumbnails img").forEach((img, i)=>{
+    img.onclick = () => {
+        const currentSlide = getCurrentSlide();
+        const nextSlide = document.querySelector(`#slides img:nth-child(${i+1})`);
+        console.log(nextSlide);
+        switchSlides(currentSlide, nextSlide);
+    }
+});
+
+
 const getCurrentSlide = () => {
     return document.querySelector("#slides :not(.hidden)");
 }
