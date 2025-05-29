1ab0b1fab5d390c65947f2d58a92e6785c48c8eb
portiaportia <portiaplante@gmail.com>
2025-05-29 11:27:44 -0400

    awer

---
+++ csce242/examples/04js/js/dog-classes.js
@@ -0,0 +1,15 @@
+class Dog {
+    constructor(title, breed, color, age, size, pic) {
+        this.title = title;
+        this.breed = breed;
+        this.color = color;
+        this.age = age;
+        this.size = size;
+        this.pic = pic;
+    }
+}
+
+const dogs = [];
+dogs.push(new Dog("Coco", "Morkie", "Black", 5, "small", "Yorkie"));
+dogs.push(new Dog("Sam", "Golden", "Yellow", 1, "Med", "golden-retriever.jpg"));
+dogs.push(new Dog("Gerald", "Pit Bull", "White", 3, "Large", "pitt-bull.jpg"));
