f0812858785dc2e5f67272ac566deb2948f82eda
portiaportia <portiaplante@gmail.com>
2025-06-02 11:10:41 -0400

    awr

--- csce242/examples/04js/js/json-parsing-breweries.js
+++ csce242/examples/04js/js/json-parsing-breweries.js
@@ -1,4 +1,15 @@
 //https://www.openbrewerydb.org/documentation
+const getMap = async(latitude, longitude) => {
+    try {
+        return (
+            await fetch(
+                `https://maps.google.com/maps?q=${latitude},${longitude}&t=&z=15&ie=UTF8&iwloc=&&output=embed`
+            )
+        ).url;
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 const getBreweries = async() => {
     const url = "https://api.openbrewerydb.org/v1/breweries";
