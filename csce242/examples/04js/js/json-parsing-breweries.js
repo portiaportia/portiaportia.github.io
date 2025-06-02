33bbff19c3e65907a5daa84446f47120cc8714f4
portiaportia <portiaplante@gmail.com>
2025-06-02 11:08:39 -0400

    awer

--- csce242/examples/04js/js/json-parsing-breweries.js
+++ csce242/examples/04js/js/json-parsing-breweries.js
@@ -1,1 +1,19 @@
 //https://www.openbrewerydb.org/documentation
+
+const getBreweries = async() => {
+    const url = "https://api.openbrewerydb.org/v1/breweries";
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    }catch(error){
+        console.log(error);
+    }
+};
+
+const showBreweries = async() => {
+    const breweries = await getBreweries();
+    console.log(breweries);
+}
+
+showBreweries();
