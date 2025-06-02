b8a03180f4e6ed3fbd48167531cc150d548f5553
portiaportia <portiaplante@gmail.com>
2025-06-02 11:32:31 -0400

    sf

---
+++ csce242/examples/04js/js/menu.js
@@ -0,0 +1,14 @@
+[
+    {
+        "name":"hawaiian pizza",
+        "sauce":"pesto",
+        "cheese": "feta",
+        "toppings":["canadian baccon", "pineapple"]
+    },
+    {
+        "name":"veggie",
+        "sauce":"tomato",
+        "cheese":"mozzarela",
+        "toppings":["green peppers", "onions", "mushrooms"]
+    }
+]
