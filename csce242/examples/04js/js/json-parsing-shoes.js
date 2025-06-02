72084ba6d4eaa0ca38725ccaceb217627ee6bd02
portiaportia <portiaplante@gmail.com>
2025-06-02 10:44:43 -0400

    awer

--- csce242/examples/04js/js/json-parsing-shoes.js
+++ csce242/examples/04js/js/json-parsing-shoes.js
@@ -1,1 +1,20 @@
 //https://portiaportia.github.io/json/shoes.json
+
+const getShoes = async() => {
+    const url = "https://portiaportia.github.io/json/shoes.json";
+
+    try {
+        const response = await fetch(url);
+        console.log(response);
+        return response.json();
+    }catch(error){
+        console.log(error);
+    }
+};
+
+const showShoes = async() => {
+   const shoes = await getShoes();
+   console.log(shoes);
+};
+
+showShoes();
