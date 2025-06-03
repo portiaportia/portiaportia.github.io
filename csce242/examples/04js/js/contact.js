8fd8fff4f41877bfeec1a39cdebc44c5695aad50
portiaportia <portiaplante@gmail.com>
2025-06-03 11:06:33 -0400

    awer

--- csce242/examples/04js/js/contact.js
+++ csce242/examples/04js/js/contact.js
@@ -1,2 +1,15 @@
 //https://web3forms.com/
 //"2cc869cb-a5d4-4b4b-b609-7d0a0bd4ea78"
+const form = document.getElementById('contact-form');
+const result = document.getElementById('result');
+
+form.onsubmit = (event) => {
+    event.preventDefault();     //don't go the action (another page)
+    
+    //collect all the name value pairs from the form
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    console.log(json);
+};
