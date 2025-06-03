968e205a9ca7576c0b513d31b11eb4924b9b3d6f
portiaportia <portiaplante@gmail.com>
2025-06-03 10:57:30 -0400

    awer

--- csce242/examples/04js/js/contact.js
+++ csce242/examples/04js/js/contact.js
@@ -1,1 +1,2 @@
 //https://web3forms.com/
+//"2cc869cb-a5d4-4b4b-b609-7d0a0bd4ea78"
--- csce242/examples/04js/js/menu.js
+++ csce242/examples/04js/js/menu.js
@@ -1,11 +1,13 @@
 [
     {
+        "_id":1,
         "name":"hawaiian pizza",
         "sauce":"pesto",
         "cheese": "feta",
         "toppings":["canadian baccon", "pineapple"]
     },
     {
+        "_id":2,
         "name":"veggie",
         "sauce":"tomato",
         "cheese":"mozzarela",
