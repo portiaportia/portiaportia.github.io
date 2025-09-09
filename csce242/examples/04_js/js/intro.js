993fa0bbe1ac879dfc334616931c58d6cefdde50
portiaportia <portiaplante@gmail.com>
2025-09-09 13:17:14 -0400

    sdfaw

---
+++ csce242/examples/04_js/js/intro.js
@@ -0,0 +1,11 @@
+/*
+const sayHello = () => {
+    console.log("Hello World");
+}
+
+document.getElementById("btn-click-me").onclick = sayHello;
+*/
+
+document.getElementById("btn-click-me").onclick = () => {
+    console.log("hello world");
+};
