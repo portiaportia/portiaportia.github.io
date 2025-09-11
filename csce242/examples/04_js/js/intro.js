d613a28feb30799d8a01f206ca571cae47b31148
portiaportia <portiaplante@gmail.com>
2025-09-11 12:16:00 -0400

    awer

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -10,3 +10,24 @@ document.getElementById("btn-click-me").onclick = (event) => {
     document.getElementById("p-welcome").innerHTML = "Hello World";
     event.currentTarget.classList.add("clicked");   //current target is the button that was clicked
 };
+
+document.getElementById("btn-happy").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "YAY";
+    pFeeling.classList.add("happy");
+    pFeeling.classList.remove("sad");
+}
+
+document.getElementById("btn-sad").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "Nay";
+    pFeeling.classList.add("sad");
+    pFeeling.classList.remove("happy");
+}
+
+document.getElementById("btn-sad").onclick = () => {
+    const pFeeling = document.getElementById("p-feeling");
+    pFeeling.innerHTML = "";
+    pFeeling.classList.remove("sad");
+    pFeeling.classList.remove("happy");
+}
