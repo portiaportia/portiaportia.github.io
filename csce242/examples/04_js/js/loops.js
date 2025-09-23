03acae6f70882040fd0d0fdb8fbe047896ad0c25
portiaportia <portiaplante@gmail.com>
2025-09-23 12:44:35 -0400

    awer

--- csce242/examples/04_js/js/loops.js
+++ csce242/examples/04_js/js/loops.js
@@ -1,4 +1,12 @@
 document.getElementById("btn-display").onclick = () => {
     const displaySection = document.getElementById("loop-result");
-    displaySection.innerHTML = "Hi";
+    const ul = document.createElement("ul");
+    displaySection.append(ul);
+
+    //loop and write the numbers one through 10
+    for(let i=0; i < 10; i++){
+        const li = document.createElement("li");
+        li.innerHTML = `${i} banana(s)`;
+        ul.append(li);
+    }
 }
