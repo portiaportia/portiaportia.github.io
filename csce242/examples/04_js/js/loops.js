8a232c5227feaba9a21576b03bcde88d06baac47
portiaportia <portiaplante@gmail.com>
2025-09-23 12:53:29 -0400

    awer

--- csce242/examples/04_js/js/loops.js
+++ csce242/examples/04_js/js/loops.js
@@ -9,4 +9,20 @@ document.getElementById("btn-display").onclick = () => {
         li.innerHTML = `${i} banana(s)`;
         ul.append(li);
     }
-}
+};
+
+document.getElementById("btn-count").onclick = () => {
+    const error = document.getElementById("error-number");
+    error.innerHTML = "";
+
+    const startNum = parseInt(document.getElementById("txt-start").value);
+    const endNum = parseInt(document.getElementById("txt-end").value);
+
+    const resultDiv = document.getElementById("d-count");
+    resultDiv.innerHTML = "";
+
+    if(startNum > endNum) {
+        error.innerHTML = "* The second number must be bigger than the first";
+        return;
+    }
+};
