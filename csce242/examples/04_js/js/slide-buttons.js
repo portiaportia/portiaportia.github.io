a805869e94a5cbce5d7090a5fe637596820280d7
portiaportia <portiaplante@gmail.com>
2025-10-07 12:27:27 -0400

    awer

--- csce242/examples/04_js/js/slide-buttons.js
+++ csce242/examples/04_js/js/slide-buttons.js
@@ -1,1 +1,25 @@
-console.log("linked");
+document.getElementById("arrow-prev").onclick = (event) =>{
+    event.preventDefault();
+    const currentSlide = document.querySelector("#slideshow :not(.hidden)");
+    let prevSlide = currentSlide.previousElementSibling;
+
+    if(!prevSlide){
+        prevSlide = document.querySelector("#slideshow :last-child");
+    }
+
+    currentSlide.classList.add("hidden");
+    prevSlide.classList.remove("hidden");
+};
+
+document.getElementById("arrow-next").onclick = (event) => {
+    event.preventDefault();
+    const currentSlide = document.querySelector("#slideshow :not(.hidden)");
+    let nextSlide = currentSlide.nextElementSibling;
+
+    if(!nextSlide) {
+        nextSlide = document.querySelector("#slideshow :first-child");
+    }
+
+    currentSlide.classList.add("hidden");
+    nextSlide.classList.remove("hidden");
+}
