f048e737c8018b2772699664942ccd40fb25b15f
portiaportia <portiaplante@gmail.com>
2025-10-07 12:31:57 -0400

    slideshow done!

--- csce242/examples/04_js/js/slide-buttons.js
+++ csce242/examples/04_js/js/slide-buttons.js
@@ -1,10 +1,10 @@
 document.getElementById("arrow-prev").onclick = (event) =>{
     event.preventDefault();
-    const currentSlide = document.querySelector("#slideshow :not(.hidden)");
+    const currentSlide = document.querySelector("#slides img:not(.hidden)");
     let prevSlide = currentSlide.previousElementSibling;
 
     if(!prevSlide){
-        prevSlide = document.querySelector("#slideshow :last-child");
+        prevSlide = document.querySelector("#slides img:last-child");
     }
 
     currentSlide.classList.add("hidden");
@@ -13,11 +13,11 @@ document.getElementById("arrow-prev").onclick = (event) =>{
 
 document.getElementById("arrow-next").onclick = (event) => {
     event.preventDefault();
-    const currentSlide = document.querySelector("#slideshow :not(.hidden)");
+    const currentSlide = document.querySelector("#slides img:not(.hidden)");
     let nextSlide = currentSlide.nextElementSibling;
 
     if(!nextSlide) {
-        nextSlide = document.querySelector("#slideshow :first-child");
+        nextSlide = document.querySelector("#slides img:first-child");
     }
 
     currentSlide.classList.add("hidden");
