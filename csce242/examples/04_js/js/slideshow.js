48d95fd709323fdc124249e70d536d28c8037e78
portiaportia <portiaplante@gmail.com>
2025-10-07 12:09:24 -0400

    awer

--- csce242/examples/04_js/js/slideshow.js
+++ csce242/examples/04_js/js/slideshow.js
@@ -1,3 +1,11 @@
 setInterval(()=>{
-    console.log("hi");
+    const currentSlide = document.querySelector("#slideshow :not(.hidden)");
+    let nextSlide = currentSlide.nextElementSibling;
+
+    if(!nextSlide){ //next slide == null
+        nextSlide = document.querySelector("#slideshow :first-child");
+    }
+
+    currentSlide.classList.add("hidden");
+    nextSlide.classList.remove("hidden");
 },1000);
