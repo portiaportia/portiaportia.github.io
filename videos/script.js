9b279addb20a0c6d6300af77e6a7638fc3217ddc
portiaportia <portiaplante@gmail.com>
2026-01-28 11:50:36 -0500

    update

--- videos/script.js
+++ videos/script.js
@@ -1,5 +1,8 @@
 const songs = [];
 songs.push(new Song("Two-Headed Boy", "Neutral Milk Hotel", "Aeroplane Over the Sea", 1998, "Folk", "neutral.webp", "TudLjZ_4VhU"));
+songs.push(new Song("Jailhouse Rock", "Elvis Presley", "Elvis's Golden Records", 1957, "Rock","jailhouse.jpeg", "hbj5uIqKHamufAhW"));
+songs.push(new Song("So What", "Miles Davis", "Kind of Blue", 1959, "Jazz","so-what.jpg", "yTHJwsuFT5kcEsNg"));
+songs.push(new Song("Jolene", "Dolly Parton", "Jolene", 1974, "Country", "jolene.jpg", "tOTUUEsCPfb0lu-8"));
 
 //on page load
 songs.forEach((song)=>{
