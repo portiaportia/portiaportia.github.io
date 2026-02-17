201d274cc4e6936304494b1c674f60cfe49febcc
portiaportia <portiaplante@gmail.com>
2026-02-17 09:11:35 -0500

    wer

--- csce242/examples/03-js/js/loops.js
+++ csce242/examples/03-js/js/loops.js
@@ -8,4 +8,26 @@ document.getElementById("btn-first-loop").onclick = () => {
     }
 };
 
-document.getElementById("btn-count-range");
+document.getElementById("btn-count-range").onclick = () => {
+    const startNumber = parseInt(document.getElementById("txt-start").value);
+    const endNumber = parseInt(document.getElementById("txt-end").value);
+    const errorP = document.getElementById("error-range");
+    errorP.innerHTML = "";
+    const divNumRange = document.getElementById("number-range");
+
+    if(startNumber > endNumber) {
+        errorP.innerHTML = "Not a valid range";
+        return;
+    }
+
+    for(let i = startNumber; i < endNumber + 1; i++){
+        const p = document.createElement("p");
+        p.innerHTML = i;
+        divNumRange.append(p);
+        p.onclick = () => {
+            document.getElementById("number-message").innerHTML = 
+                `You clicked the ${i}th item`;
+        };
+    }
+
+};
