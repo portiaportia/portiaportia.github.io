802f78361ca58664e2aea97eb539cc46a7d77183
portiaportia <portiaplante@gmail.com>
2026-02-24 08:48:43 -0500

    awer

--- csce242/examples/03-js/js/slideshow.js
+++ csce242/examples/03-js/js/slideshow.js
@@ -1,12 +1,36 @@
+//next arrow
 document.getElementById("arrow-next").onclick = (e) => {
     e.preventDefault();
-    const currentImage = document.querySelector("#slideshow img:not(.hidden)");
+    const currentImage = getCurrentSlide();
     let nextImage = currentImage.nextElementSibling;
 
     if(nextImage == null){
         nextImage = document.querySelector("#slideshow img:first-child");
     }
     
-    currentImage.classList.add("hidden");
-    nextImage.classList.remove("hidden");
+    slide(currentImage, nextImage);
+};
+
+//prev arrow
+document.getElementById("arrow-prev").onclick = (e) => {
+    e.preventDefault();
+    const currentImage = getCurrentSlide();
+    let prevImage = currentImage.previousElementSibling;
+
+    if(prevImage == null){
+        prevImage = document.querySelector("#slideshow img:last-child");
+    }
+    
+    slide(currentImage, prevImage);
+};
+
+//get current image
+const getCurrentSlide = () => {
+    return document.querySelector("#slideshow img:not(.hidden)");
+};
+
+//slide
+const slide =(current, next) => {
+    current.classList.add("hidden");
+    next.classList.remove("hidden");
 };
