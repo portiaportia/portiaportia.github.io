626f10c7fb5cb70aa76efa2a45b702fd46b837fb
portiaportia <portiaplante@gmail.com>
2026-03-03 08:50:37 -0500

    awer

--- csce242/examples/03-js/js/jsons.js
+++ csce242/examples/03-js/js/jsons.js
@@ -1,5 +1,19 @@
 //json file parsing
 //https://github.com/portiaportia/portiaportia.github.io/blob/master/json/shoes.json
 
+const getShoes = async() => {
+    const url = "https://portiaportia.github.io/json/shoes.json";
+    const response = await fetch(url);
+    return response.json();
+};
+
+const showShoes = async() => {
+    const shoes = await getShoes();
+    const shoeListDiv = document.getElementById("shoe-list");
+
+};
+
+showShoes();
+
 
 //https://openbrewerydb.org/
