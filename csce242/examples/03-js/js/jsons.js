53fdb1aac4e7967bd4ad58d3d611b38eee80f0c8
portiaportia <portiaplante@gmail.com>
2026-03-03 09:10:08 -0500

    sdf

--- csce242/examples/03-js/js/jsons.js
+++ csce242/examples/03-js/js/jsons.js
@@ -30,6 +30,17 @@ const showFish = async() => {
 
         const ul = document.createElement("ul");
         section.append(ul);
+
+        //loop through the colors
+        fish.colors.forEach((color)=>{
+            const li = document.createElement("li");
+            li.innerHTML = color;
+            ul.append(li);
+        });
+
+        const img = document.createElement("img");
+        section.prepend(img);
+        img.src =`https://portiaportia.github.io/json/${fish.img}`;
     });
 };
 
