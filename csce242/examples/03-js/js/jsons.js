1293cbb79a8ae656d689a71143e17ddd28e1de98
portiaportia <portiaplante@gmail.com>
2026-03-03 08:59:27 -0500

    wer

--- csce242/examples/03-js/js/jsons.js
+++ csce242/examples/03-js/js/jsons.js
@@ -1,19 +1,28 @@
 //json file parsing
 //https://github.com/portiaportia/portiaportia.github.io/blob/master/json/shoes.json
 
-const getShoes = async() => {
-    const url = "https://portiaportia.github.io/json/shoes.json";
+const getFish = async() => {
+    const url = "https://portiaportia.github.io/json/fish.json";
     const response = await fetch(url);
     return response.json();
 };
 
-const showShoes = async() => {
-    const shoes = await getShoes();
-    const shoeListDiv = document.getElementById("shoe-list");
+const showFish = async() => {
+    const fish = await getFish();
+    const fishListDiv = document.getElementById("fish-list");
 
+    fish.forEach((fish)=>{
+        const section = document.createElement("section");
+        section.classList.add("fish");
+        fishListDiv.append(section);
+
+        const h3 = document.createElement("h3");
+        h3.innerHTML = fish.title;
+        section.appendChild(h3);
+    });
 };
 
-showShoes();
+showFish();
 
 
 //https://openbrewerydb.org/
