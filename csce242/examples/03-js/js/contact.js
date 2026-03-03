5b01231e75113fb8f67d2ee0f0229173440f1af7
portiaportia <portiaplante@gmail.com>
2026-03-03 09:39:35 -0500

    awer

---
+++ csce242/examples/03-js/js/contact.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('contact-form');
+const result = document.getElementById('contact-result');
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+};
