fc9a033141dc0bd217a5953d90ae57252cf4e7a0
portiaportia <portiaplante@gmail.com>
2026-09-10 11:58:56 -0400

    awe

--- csce242/exercises/04js/scripts/intro.js
+++ csce242/exercises/04js/scripts/intro.js
@@ -1,4 +1,10 @@
+//Shows a message when the button is clicked
 document.getElementById("btn-show-message").onclick = (e) => {
     document.getElementById("p-message").innerHTML = "Hello World";
     e.target.innerHTML = "done!";
+};
+
+//styles the link when it's clicked
+document.getElementById("link").onclick = () => {
+    
 };
