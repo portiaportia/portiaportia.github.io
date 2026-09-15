1ac9501685e017dd952391f791535a25fdc3dd50
portiaportia <portiaplante@gmail.com>
2026-09-15 12:21:45 -0400

    awer

--- csce242/exercises/04js/scripts/intro.js
+++ csce242/exercises/04js/scripts/intro.js
@@ -20,11 +20,15 @@ document.getElementById("btn-bounce").onclick = (e) => {
 document.getElementById("txt-num-days").onkeyup = (e) => {
     const numDays = parseInt(e.target.value);
     const pMessage = document.getElementById("p-plant-msg");
+    const plantImage = document.getElementById("img-plant");
+    plantImage.classList.remove("hidden");
     
     if(numDays <= 2){
         pMessage.innerHTML = `Let your plant rest it's only been ${numDays} day(s).`;
+        plantImage.src="https://dummyimage.com/150x150/556B2F/fff&text=Happy+Plant";
     } else if(numDays <= 5){
         pMessage.innerHTML = `Time to water it's been ${numDays} days.`;
+        plantImage.src="https://dummyimage.com/150x150/BDB76B/fff&text=Thirsty";
     } else if(numDays <= 7) {
         pMessage.innerHTML = `Oh no your plant is wilting it's been ${numDays} days.`;
     } else {
