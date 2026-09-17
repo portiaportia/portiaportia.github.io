1e83e33a86330006904673ccad4549d48cdc388b
portiaportia <portiaplante@gmail.com>
2026-09-17 11:56:28 -0400

    dsfaewf

--- csce242/exercises/04js/scripts/intro.js
+++ csce242/exercises/04js/scripts/intro.js
@@ -78,11 +78,11 @@ btnStop.onclick = () =>
 setInterval(()=>{
     const pDisplay = document.getElementById("date-display");
     const today = new Date();
-    const month = today.getMonth();
-    const day = today.getDay();
+    const month = today.getMonth() + 1;
+    const day = today.getDate();
     const year = today.getFullYear();
     const seconds = today.getSeconds();
     const minutes = today.getMinutes();
     const hours = today.getHours();
-    pDisplay.innerHTML = seconds;
+    pDisplay.innerHTML = `${hours}:${minutes}:${seconds} ${month}/${day}/${year}`;
 }, 1000);
